5d0d9d0f9e227e394e118046df9870faab5f3728
Andres Puello <andres.puello@symplifica.com>
2023-07-24 17:37:09 -0500

    update listadoColaboradores

--- src/components/FormularioCurriculumRH.jsx
+++ src/components/FormularioCurriculumRH.jsx
@@ -2160,7 +2160,8 @@ const FormularioCurriculum = () => {
                 </div>
               </div>
             </div>
-            <div>
+            {operacionesExtranjera === "Si" ? (
+              <div>
               <p className="font-medium pt-5">
                 ¿En su Actividad Económica Realiza Operaciones en Moneda
                 Extranjera?
@@ -2241,6 +2242,8 @@ const FormularioCurriculum = () => {
                 <label htmlFor="otras">Otras</label>
               </div>
             </div>
+            ):null}
+            
 
             <div>
               {otras === true &&
